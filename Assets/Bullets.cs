c7667d365c0facb7c1fabdcb4d6f5c3e5b3c1e6d
GTSaikho <56368066+GTSaikho@users.noreply.github.com>
2022-01-12 17:47:15 -0300

    first commit

---
+++ Assets/Bullets.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour
+{
+
+    public float Velocidade; //Velocidade do projétil
+    public GameObject alvo; //Seleciona qual será o alvo do projétil
+    private Transform alvoT; //Posição que vai receber a posição do alvo
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        alvoT = alvo.transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float VelocidadeConstante = Velocidade * Time.deltaTime;
+
+
+        transform.position = Vector3.MoveTowards(transform.position, alvoT.position, VelocidadeConstante/10);
+        transform.right = alvoT.position - transform.position;
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Heart") || collision.gameObject.CompareTag("Shield"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    
+}
---
+++ Assets/Controls.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+
+    private RaycastHit tocado;
+    private GameObject toque;
+    public GameObject Shield;
+    public GameObject ShieldFront;
+    public GameObject Slash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShieldFront.SetActive(false);
+        Slash.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out tocado))
+            {
+                toque = tocado.collider.gameObject;
+                                                              
+                switch (toque.name)
+                {
+                    case "LeftT":
+                        Shield.SetActive(true);
+                        Shield.transform.position = new Vector3(-3.5f, 1, 0);
+                        Shield.transform.rotation = Quaternion.Euler(0, 0, 180);
+                        ShieldFront.SetActive(false);
+                        break;
+
+                    case "RightT":
+                        Shield.SetActive(true);
+                        Shield.transform.position = new Vector3(3.2f, 1, 0);
+                        Shield.transform.rotation = Quaternion.Euler(0, 0, 0);
+                        ShieldFront.SetActive(false);
+                        break;
+
+                    case "UpT":
+                        Shield.SetActive(true);
+                        Shield.transform.position = new Vector3(-0.1f, 4.5f, 0);
+                        Shield.transform.rotation = Quaternion.Euler(0, 0, 90);
+                        ShieldFront.SetActive(false);
+                        break;
+
+                    case "DownT":
+
+                        Shield.SetActive(true);
+                        Shield.transform.position = new Vector3(-0.1f, -2.5f, 0);
+                        Shield.transform.rotation = Quaternion.Euler(0, 0, -90);
+                        ShieldFront.SetActive(false);                        
+                        break;
+
+                    case "":
+                        ShieldFront.SetActive(true);
+                        Shield.SetActive(false);
+                        break;
+
+
+                }
+
+            }
+        }
+
+    }
+}
